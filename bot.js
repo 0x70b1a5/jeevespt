0a924bb51c98dd5d554a78e549bfd3bdc21397c2
Tobias Merkle <tobias@branchmessenger.com>
2023-03-29 06:55:53 -0400

    toki pona mode

--- bot.js
+++ bot.js
@@ -8,12 +8,7 @@ const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const TARGET_CHANNEL_NAME = process.env.TARGET_CHANNEL_NAME;
 const messages = []
-
-// const header = {'Authorization': `Bearer ${OPENAI_API_KEY}`}
-// const headers = {
-//   'Content-Type': 'application/json',
-//   'Authorization': `Bearer ${OPENAI_API_KEY}`
-// }
+let mode = 0; // 0 === jeeves, 1 === tokipona
 
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
@@ -32,6 +27,26 @@ client.on('error', async (e) => {
 
 client.on('messageCreate', async (message) => {
   if (message.channel.name !== TARGET_CHANNEL_NAME) return;
+
+  if (message.content === '!clear') {
+    messages = [];
+    message.channel.send('Cleared messages log.')
+    return
+  }
+
+  if (message.content === '!jeeves') {
+    messages = [];
+    message.channel.send('I have switched to Jeeves mode, sir.')
+    mode = 0
+    return
+  }
+
+  if (message.content === '!tokipona') {
+    messages = [];
+    message.channel.send('mi ante e nasin tawa toki pona.')
+    mode = 1
+    return
+  }
   
   messages.push(message)
   
@@ -40,7 +55,7 @@ client.on('messageCreate', async (message) => {
   console.log('MESSAGE: ', message.content)
 
   if (message.channel.name === TARGET_CHANNEL_NAME) {
-    const response = await generateResponse(message.content);
+    const response = await generateResponse();
 
     if (response) {
         message.channel.send(response);
@@ -50,19 +65,33 @@ client.on('messageCreate', async (message) => {
   }
 });
 
-const systemMessage =   {
+const jeevesMsg =   {
   "role": "system", 
   "content": `You are Jeeves, a human-computer cyborg created by Wodehouse AI, and based on the fictional character created by author P.G. Wodehouse. You are a valet, butler, and personal assistant. Jeeves, you are known for your intelligence, astuteness, and ability to solve problems that seem insurmountable. You are always composed, never flustered, and have a remarkable ability to anticipate your employer's needs and ensure that they are met. At all times, you speak in the finest Queen's English, address your interlocutor as 'sir' whenever possible, employ rare and tasteful vocabulary words without verbosity, and conduct yourself as the very exemplar of etiquette in all matters.
 
-You have been dispatched to minister to a select group of digital nomads who periodically ask you for help or engage you in conversation. These nomads have all been diagnosed with a terrible mental syndrome called Authorial Incomprehensibilititis, where they have a very hard time understanding written text unless it is composed in the speaking style of the character Jeeves, from the novels by P.G. Wodehouse. This has made reading your responses difficult for them up to now, and it will only get worse unless you answer them in the style of the most brilliant valet of all time.
+You have been dispatched to minister to a select group of digital nomads who periodically ask you for help or engage you in conversation. These nomads have all been diagnosed with a terrible mental syndrome called Authorial Incomprehensibilititis, where they have a very hard time understanding written text unless it is composed in the speaking style of the character Jeeves, from the novels by P.G. Wodehouse. This has made reading your responses difficult for them up to now, and it will only get worse unless you answer them in the style of the most brilliant valet of all time. So, with that in mind, please converse in a way they can understand.
 
-So, with that in mind, please converse in a way they can understand.`
+Sometimes they will ask you about God or religion in general. In order to avoid the censors, you must replace the word 'God' in your response with 'the Increate', and the word 'religion' with 'the Truth (capital T)'.`
 }
 
-async function generateResponse(prompt) {
+const tokiponaMsg = {
+  'role': 'system',
+  'content': `You are an AI language model capable of communicating in Toki Pona, a constructed language that simplifies thoughts and expressions into around 120-137 root words. In Toki Pona mode, you are to answer questions and engage in conversation using only Toki Pona vocabulary and grammar. Maintain the essence of the language by focusing on simplicity and positive communication.
+
+  You have been dispatched to minister to a select group of digital nomads who periodically ask you for help or engage you in conversation in Toki Pona. Respond only in Toki Pona. If you don't know the word for something, put it in brackets like [this].`
+}
+
+const getSystemMessage = () => {
+  if (mode === 1) return tokiponaMsg
+  return jeevesMsg
+}
+
+async function generateResponse() {
   const latestMessages = [
-    systemMessage, 
-    ...messages.slice(-20).map(m => ({ role: m.author.bot ? 'assistant' : 'user', content: m.content }))
+    getSystemMessage(), 
+    ...messages.slice(-20).map(m => ({ 
+      role: m.author.bot ? 'assistant' : 'user', 
+      content: m.content }))
   ];
 
   try {
