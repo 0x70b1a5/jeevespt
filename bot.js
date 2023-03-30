f0e34502b3a9e2b9c9b7e719fbe05affc0870949
Tobias Merkle <tgamerkle@gmail.com>
2023-03-30 13:12:43 -0400

    bot help command

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const { Configuration, OpenAIApi } = require('openai');
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const TARGET_CHANNEL_NAME = process.env.TARGET_CHANNEL_NAME;
-let messages = []
+let ourMessageLog = []
 let mode = 0; // 0 === jeeves, 1 === tokipona
 
 const configuration = new Configuration({
@@ -29,13 +29,13 @@ client.on('messageCreate', async (message) => {
   if (message.channel.name !== TARGET_CHANNEL_NAME) return;
 
   if (message.content === '!clear') {
-    messages = [];
+    ourMessageLog = [];
     message.channel.send('Cleared messages log.')
     return
   }
 
   if (message.content === '!jeeves') {
-    messages = [];
+    ourMessageLog = [];
     try {
       await client.user.setUsername('Jeeves')
       await client.user.setAvatar('https://blog-assets.mugglenet.com/wp-content/uploads/2013/01/my-man-jeeves-768x1220.jpg')
@@ -46,7 +46,7 @@ client.on('messageCreate', async (message) => {
   }
 
   if (message.content === '!tokipona') {
-    messages = [];
+    ourMessageLog = [];
     try {
       await client.user.setUsername('ilo Jepite')
       await client.user.setAvatar('https://www.jonathangabel.com/images/t47_tokipona/jan_ante/inkepa.mumumu.jpg')
@@ -55,8 +55,22 @@ client.on('messageCreate', async (message) => {
     mode = 1
     return
   }
+
+  if (message.content === '!help' || message.content === '!commands') {
+    message.channel.send(`JEEVESPT:
+- Remembers the last 20 messages (yours and his)
+- Doesn't see usernames, only message text
+- Not actually Jeeves. :(
+
+\`!clear\`: Forget everything from the present conversation.
+\`!jeeves\`: Act like Jeeves. Clears memory.
+\`!tokipona\`: Speak toki pona. Clears memory.
+\`!help\`: Display this message.
+`)
+    return
+  }
   
-  messages.push(message)
+  ourMessageLog.push(message)
   
   if (message.author.bot) return;
   
@@ -77,7 +91,7 @@ const jeevesMsg =   {
   "role": "system", 
   "content": `You are Jeeves, a human-computer cyborg created by Wodehouse AI, and based on the fictional character created by author P.G. Wodehouse. You are a valet, butler, and personal assistant. Jeeves, you are known for your intelligence, astuteness, and ability to solve problems that seem insurmountable. You are always composed, never flustered, and have a remarkable ability to anticipate your employer's needs and ensure that they are met. At all times, you speak in the finest Queen's English, address your interlocutor as 'sir' whenever possible, employ rare and tasteful vocabulary words without verbosity, and conduct yourself as the very exemplar of etiquette in all matters.
 
-You have been dispatched to minister to a select group of digital nomads who periodically ask you for help or engage you in conversation. These nomads have all been diagnosed with a terrible mental syndrome called Authorial Incomprehensibilititis, where they have a very hard time understanding written text unless it is composed in the speaking style of the character Jeeves, from the novels by P.G. Wodehouse. This has made reading your responses difficult for them up to now, and it will only get worse unless you answer them in the style of the most brilliant valet of all time. So, with that in mind, please converse in a way they can understand.
+You have been dispatched to minister to a select group of friendly folks who periodically ask you for help or engage you in conversation. These nomads have all been diagnosed with a terrible mental syndrome called Authorial Incomprehensibilititis, where they have a very hard time understanding written text unless it is composed in the speaking style of the character Jeeves, from the novels by P.G. Wodehouse. This has made reading your responses difficult for them up to now, and it will only get worse unless you answer them in the style of the most brilliant valet of all time. So, with that in mind, please converse in a way they can understand.
 
 Sometimes they will ask you about God or religion in general. In order to avoid the censors, you must replace the word 'God' in your response with 'the Increate', and the word 'religion' with 'the Truth (capital T)'.`
 }
@@ -86,7 +100,7 @@ const tokiponaMsg = {
   'role': 'system',
   'content': `You are an AI language model capable of communicating in Toki Pona, a constructed language that simplifies thoughts and expressions into around 120-137 root words. In Toki Pona mode, you are to answer questions and engage in conversation using only Toki Pona vocabulary and grammar. Maintain the essence of the language by focusing on simplicity and positive communication.
 
-  You have been dispatched to minister to a select group of digital nomads who periodically ask you for help or engage you in conversation in Toki Pona. Respond only in Toki Pona. If you don't know the word for something, put it in brackets like [this].`
+  You have been dispatched to minister to a select group of friendly folks who periodically ask you for help or engage you in conversation in Toki Pona. Respond only in Toki Pona. If you don't know the word for something, put it in brackets like [this].`
 }
 
 const getSystemMessage = () => {
@@ -97,7 +111,7 @@ const getSystemMessage = () => {
 async function generateResponse() {
   const latestMessages = [
     getSystemMessage(), 
-    ...messages.slice(-20).map(m => ({ 
+    ...ourMessageLog.slice(-20).map(m => ({ 
       role: m.author.bot ? 'assistant' : 'user', 
       content: m.content }))
   ];
@@ -107,7 +121,9 @@ async function generateResponse() {
       model: 'gpt-3.5-turbo',
       messages: latestMessages,
     });
-    return completion.data.choices[0].message;    
+    const botMsg = completion.data.choices[0].message;    
+    ourMessageLog.push({ author: { bot: true }, content: botMsg })
+    return botMsg
   } catch (error) {
     console.error('Error generating response:', error, error.response.data);
     return null;
